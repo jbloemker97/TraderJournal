42f3813783b2b871716229f06e25201d8eac2db8
Jacob Bloemker <jbloemker97@gmail.com>
2019-09-06 07:43:10 -0400

    Added navbar and bootstrap jquery

--- client/src/components/global/NavBar.vue
+++ client/src/components/global/NavBar.vue
@@ -1,5 +1,29 @@
 <template>
-    <h1>NavBar</h1>
+    <div>
+        <nav class="navbar navbar-expand-sm navbar-light bg-faded">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav-content" aria-controls="nav-content" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- Brand -->
+            <a class="navbar-brand" href="#">Logo</a>
+
+            <!-- Links -->
+            <div class="collapse navbar-collapse justify-content-end" id="nav-content">   
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Link 1</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Link 2</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Link 3</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
 </template>
 
 <script>
@@ -9,5 +33,7 @@ export default {
 </script>
 
 <style scoped>
-
+    nav {
+        background-color: #0377fc;
+    }
 </style>
